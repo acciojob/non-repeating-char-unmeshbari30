30a4bad7943213e3508ca995ce0d8a2c4cd2d21a
Unmesh Bari <unmeshbari5@gmail.com>
2025-11-21 00:02:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function firstNonRepeatedChar(str) {
+	if(!str) return null
  // Write your code here
 	let hMap= {}
 
-	for(ch in str){
+	for(let ch in str){
 		hMap[ch] = (hMap[ch] || 0) + 1
 	}
 
-	for(ch in str){
+	for(let ch in str){
 		if(hMap[ch] == 1){
 			return ch
 		}
