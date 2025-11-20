1dba294da2296760178768fa868802a18ae3e875
Unmesh Bari <unmeshbari5@gmail.com>
2025-11-20 23:46:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
-	 for(let i=0; i<str.length-1; i++){
+	 for(let i=0; i<str.length-1; i+=2){
 		 let first = str[i]
 		 let second = str[i+1]
 		 if(first !== second){
 			 return first
-		 }else{
-			 return null;
 		 }
 	 }
+	return null
+	
 }
-const input = prompt("Enter a string");
+// const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
